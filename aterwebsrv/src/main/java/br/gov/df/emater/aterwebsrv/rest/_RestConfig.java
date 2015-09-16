e76ca5e4e69d4b506ec36323d85002c1c30b6e9c
Fernando Frazão <ffrazao@users.noreply.github.com>
2015-09-16 13:54:17 -0300

    Início do fim da instalação da segurança no REST

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/rest/_RestConfig.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/rest/_RestConfig.java
@@ -1,29 +1,61 @@
 package br.gov.df.emater.aterwebsrv.rest;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "br.gov.df.emater.aterwebsrv" })
-public class _RestConfig extends WebMvcConfigurerAdapter {
+public class _RestConfig extends DelegatingWebMvcConfiguration {
 
 	@Override
-	public void configureMessageConverters(
-			List<HttpMessageConverter<?>> converters) {
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+
+		MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
+		defaultView.setExtractValueFromSingleKeyModel(true);
+
+		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+		contentViewResolver.setViewResolvers(Arrays.<ViewResolver> asList(viewResolver));
+		contentViewResolver.setDefaultViews(Arrays.<View> asList(defaultView));
+		return contentViewResolver;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
 		builder.serializationInclusion(Include.NON_NULL);
 		builder.serializationInclusion(Include.NON_EMPTY);
@@ -31,10 +63,8 @@ public class _RestConfig extends WebMvcConfigurerAdapter {
 		builder.failOnEmptyBeans(false);
 		builder.failOnUnknownProperties(false);
 
-		builder.indentOutput(true).dateFormat(
-				new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss"));
-		converters
-				.add(new MappingJackson2HttpMessageConverter(builder.build()));
+		builder.indentOutput(true).dateFormat(new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss"));
+		converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
 	}
 
 	@Bean(name = "multipartResolver")
@@ -56,7 +86,7 @@ public class _RestConfig extends WebMvcConfigurerAdapter {
 		registrationBean.setFilter(characterEncodingFilter);
 		return registrationBean;
 	}
-	
+
 	// TODO Habilitar o log4j
 
 }
