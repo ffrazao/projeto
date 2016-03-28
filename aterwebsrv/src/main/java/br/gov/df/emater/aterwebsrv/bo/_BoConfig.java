5ccec3d0ef9779e2e235978a70ea8b47a1ee7187
Fernando Frazão <ffrazao@users.noreply.github.com>
2016-03-28 15:38:43 -0300

    ajuste em _boconfig.java que estava com anotacao WebAppConfiguration desnecessaria

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/_BoConfig.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/_BoConfig.java
@@ -2,11 +2,9 @@ package br.gov.df.emater.aterwebsrv.bo;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @Configuration
 @ComponentScan(basePackages = { "br.gov.df.emater.aterwebsrv.bo" })
-@WebAppConfiguration
 public class _BoConfig {
 
 }
