131178501cce6a1e886489f066555db607ecb157
Fernando Frazão <ffrazao@users.noreply.github.com>
2016-04-10 22:46:07 -0300

    arquivo less

--- aterweb/Gruntfile.js
+++ aterweb/Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
     connect: {
       main: {
         options: {
-          port: 80,
+          port: 9001,
           hostname: '0.0.0.0'
         }
       }
