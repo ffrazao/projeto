fc48ea7f55d2f012fe1e523adfb6c9548cdba2ac
Fernando Frazão <ffrazao@users.noreply.github.com>
2016-07-24 22:48:11 -0300

    projeto de credito rural, ajustes

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/credito_rural/ProjetoCreditoRuralDaoCustom.java
+++
@@ -1,8 +0,0 @@
-package br.gov.df.emater.aterwebsrv.dao.credito_rural;
-
-import br.gov.df.emater.aterwebsrv.dao._FiltrarCustom;
-import br.gov.df.emater.aterwebsrv.modelo.dto.ProjetoCreditoRuralCadFiltroDto;
-
-public interface ProjetoCreditoRuralDaoCustom extends _FiltrarCustom<ProjetoCreditoRuralCadFiltroDto> {
-
-}
