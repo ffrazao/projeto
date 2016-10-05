6e466ce68d62fae16f21c57ba7de82799480a670
Fernando Frazão <ffrazao@users.noreply.github.com>
2016-10-05 16:34:45 -0300

    Ajustes no codigo para pegar arquivos do sistema

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/pessoa/VerifPendDatasCmd.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/pessoa/VerifPendDatasCmd.java
@@ -5,22 +5,15 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.gov.df.emater.aterwebsrv.bo._Contexto;
 import br.gov.df.emater.aterwebsrv.bo.pendencia.VerificarPendenciasCmd;
-import br.gov.df.emater.aterwebsrv.dao.pessoa.PessoaFisicaDao;
-import br.gov.df.emater.aterwebsrv.dao.pessoa.PessoaRelacionamentoDao;
 import br.gov.df.emater.aterwebsrv.modelo.dominio.PendenciaCodigo;
 import br.gov.df.emater.aterwebsrv.modelo.pessoa.PessoaFisica;
-import br.gov.df.emater.aterwebsrv.modelo.pessoa.PessoaJuridica;
 
 @Service("PessoaVerifPendDatasCmd")
 public class VerifPendDatasCmd extends VerificarPendenciasCmd {
-
-	@Autowired
-	private PessoaFisicaDao pessoaFisicaDao;
 
 	@Override
 	public String constatarPendencia(_Contexto contexto) {
@@ -28,20 +21,19 @@ public class VerifPendDatasCmd extends VerificarPendenciasCmd {
 			return null;
 		}
 		PessoaFisica pessoa = (PessoaFisica) getPendenciavel();
-		
+
 		Calendar dataNascimento = pessoa.getNascimento();
 		List<String> pendenciaList = new ArrayList<>();
 
-			
-			// TODO fazer verificacao no proprio registro se pf ver em seus relac.
-			// se relac ver principa e outros relacs
-			if (dataNascimento != null) {
-				if (dataNascimento.after(Calendar.getInstance())) {
-					 pendenciaList.add(String.format("A data de nascimento informada é inválida\n"));
-				} 
+		// TODO fazer verificacao no proprio registro se pf ver em seus relac.
+		// se relac ver principa e outros relacs
+		if (dataNascimento != null) {
+			if (dataNascimento.after(Calendar.getInstance())) {
+				pendenciaList.add(String.format("A data de nascimento informada é inválida\n"));
 			}
-			
-			return extraiResultado(pendenciaList);
+		}
+
+		return extraiResultado(pendenciaList);
 	}
 
 	@Override
--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/util/ArquivoDescerCmd.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/bo/util/ArquivoDescerCmd.java
@@ -1,6 +1,7 @@
 package br.gov.df.emater.aterwebsrv.bo.util;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,6 +12,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +22,15 @@ import br.gov.df.emater.aterwebsrv.bo._Contexto;
 import br.gov.df.emater.aterwebsrv.dao.pessoa.ArquivoDao;
 import br.gov.df.emater.aterwebsrv.modelo.dominio.ArquivoTipo;
 import br.gov.df.emater.aterwebsrv.modelo.pessoa.Arquivo;
+import eu.medsea.mimeutil.MimeUtil;
 
 @Service("UtilArquivoDescerCmd")
 public class ArquivoDescerCmd extends _Comando implements ArquivoConstantes {
+
+	class ArquivoConteudo {
+		File arquivo;
+		byte[] conteudo;
+	}
 
 	@Autowired
 	private ArquivoDao arquivoDao;
@@ -30,73 +38,85 @@ public class ArquivoDescerCmd extends _Comando implements ArquivoConstantes {
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean executar(_Contexto context) throws Exception {
-
 		// recuperar parametros
 		Map<String, Object> requisicao = (Map<String, Object>) context.getRequisicao();
-		String file = (String) requisicao.get("arquivo");
+		String nomeArquivo = (String) requisicao.get("arquivo");
 		HttpServletRequest request = (HttpServletRequest) requisicao.get("request");
 		HttpServletResponse response = (HttpServletResponse) requisicao.get("response");
-
-		String diretorio = null;
-
 		try {
-			Arquivo arquivoBanco = arquivoDao.findByMd5(file);
-			if (arquivoBanco == null) {
-				throw new FileNotFoundException(String.format("O arquivo [%s] não foi encontrado", file));
-			} else {
-
-				if (ArquivoTipo.P.equals(arquivoBanco.getTipo())) {
-					diretorio = DIRETORIO_PERFIL;
-				} else if (ArquivoTipo.A.equals(arquivoBanco.getTipo())) {
-					diretorio = DIRETORIO_UPLOAD;
-				}
-				File diretorioServidor = new File(request.getServletContext().getRealPath("/").concat(diretorio));
-				
-				if (!diretorioServidor.exists()) {
-					logger.debug(String.format("Diretório de arquivos não encontrado, criando um novo [%s]", diretorioServidor.toString()));
-					diretorioServidor.mkdirs();
-				}
-
-				File arquivoDescer = new File(diretorioServidor, String.format("%s%s", arquivoBanco.getMd5(), arquivoBanco.getExtensao()));
-
-				// recuperar o arquivo no system file
-				if (!arquivoDescer.exists()) {
-					logger.debug(String.format("O arquivo [%s] não encontrado no servidor de arquivos, inicio do download do banco de dados", file));
-					if (arquivoBanco.getConteudo() == null || arquivoBanco.getConteudo().length == 0) {
-						throw new FileNotFoundException(String.format("Nao foi possivel recuperar o arquivo [%s]", arquivoBanco.getMd5()));
-					}
-
-					// recuperar o conteúdo do arquivo caso tenha sido apagado
-					try (OutputStream out = new FileOutputStream(arquivoDescer)) {
-						out.write(arquivoBanco.getConteudo());
-						out.flush();
-						logger.debug(String.format("O arquivo [%s] salvo em [%s]", file, arquivoDescer.getAbsolutePath()));
-					}
-				}
-
-				// ler o conteudo do arquivo
-				byte[] bytes = new byte[(int) arquivoDescer.length()];
-				try (InputStream in = new FileInputStream(arquivoDescer)) {
-					in.read(bytes);
-				}
-
-				// Preparar a resposta
-				// response.setHeader("Content-Disposition",
-				// String.format("attachment; filename=%s",
-				// arquivo.getNomeOriginal()));
-				response.setHeader("Content-Disposition", String.format("inline; filename=%s", arquivoBanco.getNomeOriginal()));
-				response.setContentType(arquivoBanco.getMimeTipo());
-				response.setContentLength(arquivoBanco.getTamanho());
-				response.getOutputStream().write(bytes);
-				response.flushBuffer();
-
-				arquivoBanco = null;
+			ArquivoConteudo conteudo = null;
+			conteudo = getDiretorio(request.getServletContext().getRealPath("/").concat(DIRETORIO_PERFIL), nomeArquivo);
+			if (conteudo == null) {
+				conteudo = getDiretorio(request.getServletContext().getRealPath("/").concat(DIRETORIO_UPLOAD), nomeArquivo);
 			}
+			if (conteudo == null) {
+				Arquivo arquivoBanco = arquivoDao.findByMd5(nomeArquivo);
+				if (arquivoBanco == null) {
+					throw new FileNotFoundException(String.format("O arquivo [%s] não foi encontrado", nomeArquivo));
+				} else {
+					String diretorio = null;
+					if (ArquivoTipo.P.equals(arquivoBanco.getTipo())) {
+						diretorio = DIRETORIO_PERFIL;
+					} else if (ArquivoTipo.A.equals(arquivoBanco.getTipo())) {
+						diretorio = DIRETORIO_UPLOAD;
+					}
+					File diretorioServidor = new File(request.getServletContext().getRealPath("/").concat(diretorio));
+					if (!diretorioServidor.exists()) {
+						logger.debug(String.format("Diretório de arquivos não encontrado, criando um novo [%s]", diretorioServidor.toString()));
+						diretorioServidor.mkdirs();
+					}
+					File arquivoDescer = new File(diretorioServidor, String.format("%s%s", arquivoBanco.getMd5(), arquivoBanco.getExtensao()));
 
-			return false;
+					// recuperar o arquivo no system file
+					if (!arquivoDescer.exists()) {
+						logger.debug(String.format("O arquivo [%s] não encontrado no servidor de arquivos, inicio do download do banco de dados", nomeArquivo));
+						if (arquivoBanco.getConteudo() == null || arquivoBanco.getConteudo().length == 0) {
+							throw new FileNotFoundException(String.format("Nao foi possivel recuperar o arquivo [%s]", arquivoBanco.getMd5()));
+						}
+						// recuperar o conteúdo do arquivo caso tenha sido
+						// apagado
+						try (OutputStream out = new FileOutputStream(arquivoDescer)) {
+							out.write(arquivoBanco.getConteudo());
+							out.flush();
+							logger.debug(String.format("O arquivo [%s] salvo em [%s]", nomeArquivo, arquivoDescer.getAbsolutePath()));
+						}
+					}
+					conteudo = new ArquivoConteudo();
+					conteudo.arquivo = arquivoDescer;
+					conteudo.conteudo = arquivoBanco.getConteudo();
+				}
+			}
+			// identificar mimetype
+			response.setHeader("Content-Disposition", String.format("inline; filename=%s", conteudo.arquivo.getName()));
+			response.setContentType(MimeUtil.getMimeTypes(conteudo.conteudo).toString());
+			response.setContentLength(conteudo.conteudo.length);
+			response.getOutputStream().write(conteudo.conteudo);
+			response.flushBuffer();
+
 		} catch (Exception e) {
 			new BoException(e);
 		}
 		return false;
 	}
+
+	private ArquivoConteudo getDiretorio(String diretorioNome, String arquivoBusca) throws Exception {
+		ArquivoConteudo result = null;
+		File diretorio = new File(diretorioNome);
+		if (diretorio.exists()) {
+			FileFilter fileFilter = new RegexFileFilter(arquivoBusca.concat(".*"));
+			for (File arquivo : diretorio.listFiles(fileFilter)) {
+				if (arquivo.exists()) {
+					result = new ArquivoConteudo();
+					result.arquivo = arquivo;
+					result.conteudo = new byte[(int) arquivo.length()];
+					try (InputStream in = new FileInputStream(arquivo)) {
+						in.read(result.conteudo);
+					}
+					return result;
+				}
+			}
+		}
+		return result;
+	}
+
 }
