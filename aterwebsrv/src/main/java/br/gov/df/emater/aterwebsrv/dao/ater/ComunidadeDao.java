4ebbc0888dfd31c17c71f30451b27489c02cc31d
Fernando Frazão <ffrazao@users.noreply.github.com>
2016-12-12 16:31:45 -0200

    remoção do cache, deixou de encontrar registros na base

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/ater/ComunidadeDao.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/ater/ComunidadeDao.java
@@ -3,7 +3,6 @@ package br.gov.df.emater.aterwebsrv.dao.ater;
 import java.util.List;
 import java.util.Set;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,16 +11,16 @@ import br.gov.df.emater.aterwebsrv.modelo.ater.Comunidade;
 @Repository("ComunidadeDao")
 public interface ComunidadeDao extends JpaRepository<Comunidade, Integer> {
 
-	@Cacheable("findByNomeLikeOrderByNomeAsc")
+	//@Cacheable("findByNomeLikeOrderByNomeAsc")
 	List<Comunidade> findByNomeLikeOrderByNomeAsc(String nomeLike);
 
-	@Cacheable("findByNomeOrderByNomeAsc")
+	//@Cacheable("findByNomeOrderByNomeAsc")
 	List<Comunidade> findByNomeOrderByNomeAsc(String nome);
 
-	@Cacheable("findByUnidadeOrganizacionalIdInAndNomeLikeOrderByNomeAsc")
+	//@Cacheable("findByUnidadeOrganizacionalIdInAndNomeLikeOrderByNomeAsc")
 	List<Comunidade> findByUnidadeOrganizacionalIdInAndNomeLikeOrderByNomeAsc(Set<Integer> unidadeOrganizacionalList, String nomeLike);
 
-	@Cacheable("findByUnidadeOrganizacionalPessoaJuridicaIdInAndNomeLikeOrderByNomeAsc")
+	//@Cacheable("findByUnidadeOrganizacionalPessoaJuridicaIdInAndNomeLikeOrderByNomeAsc")
 	List<Comunidade> findByUnidadeOrganizacionalPessoaJuridicaIdInAndNomeLikeOrderByNomeAsc(Set<Integer> pessoaJuridicaList, String nomeLike);
 
 }
