597a4b195e1e2ab0424a2c310ff5a057d9482b80
ajuniofc <a.junio.fc@gmail.com>
2017-01-24 13:11:20 -0200

    Ajuste na carteira do produtor e criação da declaração do produtor

--- aterweb/app.js
+++ aterweb/app.js
@@ -1473,6 +1473,52 @@ var TIMEOUT_TEMPO = 10 * 60;
                 };
             }]);
 
+
+
+angular.module(pNmModulo).directive('maxlines', function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, elem, attrs, ngModel) {
+      var maxLines = 1;
+      attrs.$observe('maxlines', function(val) {
+        maxLines = parseInt(val);
+      });
+      ngModel.$validators.maxlines = function(modelValue, viewValue) {
+        var numLines = (modelValue || '').split("\n").length;
+        return numLines <= maxLines;
+      };
+
+      function addKeypress() {
+        elem.on('keypress', function(event) {
+          // test if adding a newline would cause the validator to fail
+          if (event.keyCode === 13 && !ngModel.$validators.maxlines(ngModel.$modelValue + '\n', ngModel.$viewValue + '\n')) {
+            event.preventDefault();
+          }
+        });
+      }
+
+      function removeKeypress() {
+        elem.off('.maxlines');
+      }
+      
+      attrs.$observe('maxlinesPreventEnter', function(preventEnter) {
+        // if attribute value starts with 'f', treat as false. Everything else is true
+        preventEnter = (preventEnter || '').toLocaleLowerCase().indexOf('f') !== 0;
+        if (preventEnter) {
+          addKeypress();
+        } else {
+          removeKeypress();
+        }
+      });
+
+      
+
+      scope.$on('$destroy', removeKeypress);
+    }
+  };
+});
+
 })('principal', null, 'Módulo Principal do Sistema');
 
 var criarEstadosPadrao = function(stateProvider, nomeModulo, nomeController, urlModulo) {
