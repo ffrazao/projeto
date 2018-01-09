c2869c11e167cb1bd329afc2cf664371579c24a4
Adan <adansks@gmail.com>
2018-01-09 15:22:09 -0200

    Correção na Declaracao Ceasa.

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dto/pessoa/DeclaracaoCeasaRelFiltroDto.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dto/pessoa/DeclaracaoCeasaRelFiltroDto.java
@@ -4,30 +4,43 @@ import java.util.List;
 
 import br.gov.df.emater.aterwebsrv.dto.CadFiltroDtoCustom;
 import br.gov.df.emater.aterwebsrv.modelo.ater.PublicoAlvoPropriedadeRural;
+import br.gov.df.emater.aterwebsrv.modelo.indice_producao.BemClassificado;
 import br.gov.df.emater.aterwebsrv.modelo.pessoa.Pessoa;
 
 public class DeclaracaoCeasaRelFiltroDto extends CadFiltroDtoCustom {
 
 	private static final long serialVersionUID = 1L;
 
-	private DeclaracaoProdutorRelFiltroDto declaracaoProdutorRelFiltroDto;
+	//private DeclaracaoProdutorRelFiltroDto declaracaoProdutorRelFiltroDto;
 	
-	private List<Object> producaoList;
+	private List<Integer> publicoAlvoPropriedadeRuralIdList;
+	
+	private List<DeclaracaoCeasaProdutoRelDto> producaoList;
+	
+	
 
-	public DeclaracaoProdutorRelFiltroDto getDeclaracaoProdutorRelFiltroDto() {
-		return declaracaoProdutorRelFiltroDto;
-	}
+//	public DeclaracaoProdutorRelFiltroDto getDeclaracaoProdutorRelFiltroDto() {
+//		return declaracaoProdutorRelFiltroDto;
+//	}
+//
+//	public void setDeclaracaoProdutorRelFiltroDto(DeclaracaoProdutorRelFiltroDto declaracaoProdutorRelFiltroDto) {
+//		this.declaracaoProdutorRelFiltroDto = declaracaoProdutorRelFiltroDto;
+//	}
 
-	public void setDeclaracaoProdutorRelFiltroDto(DeclaracaoProdutorRelFiltroDto declaracaoProdutorRelFiltroDto) {
-		this.declaracaoProdutorRelFiltroDto = declaracaoProdutorRelFiltroDto;
-	}
-
-	public List<Object> getProducaoList() {
+	public List<DeclaracaoCeasaProdutoRelDto> getProducaoList() {
 		return producaoList;
 	}
 
-	public void setProducaoList(List<Object> producaoList) {
+	public void setProducaoList(List<DeclaracaoCeasaProdutoRelDto> producaoList) {
 		this.producaoList = producaoList;
+	}
+
+	public List<Integer> getPublicoAlvoPropriedadeRuralIdList() {
+		return publicoAlvoPropriedadeRuralIdList;
+	}
+
+	public void setPublicoAlvoPropriedadeRuralIdList(List<Integer> publicoAlvoPropriedadeRuralIdList) {
+		this.publicoAlvoPropriedadeRuralIdList = publicoAlvoPropriedadeRuralIdList;
 	}
 
 	
