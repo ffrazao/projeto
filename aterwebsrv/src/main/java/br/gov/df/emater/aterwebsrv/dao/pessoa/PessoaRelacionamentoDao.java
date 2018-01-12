1670badbe2991ee413ae5deefc6ce217763fbc3c
Adan <adansks@gmail.com>
2018-01-12 10:24:27 -0200

    Implementação da opção Transformar Cadastro em Completo.

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/pessoa/PessoaRelacionamentoDao.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/pessoa/PessoaRelacionamentoDao.java
@@ -3,10 +3,13 @@ package br.gov.df.emater.aterwebsrv.dao.pessoa;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.gov.df.emater.aterwebsrv.modelo.pessoa.Pessoa;
 import br.gov.df.emater.aterwebsrv.modelo.pessoa.PessoaRelacionamento;
 import br.gov.df.emater.aterwebsrv.modelo.pessoa.Relacionamento;
 
@@ -24,5 +27,10 @@ public interface PessoaRelacionamentoDao extends JpaRepository<PessoaRelacioname
 	@Query("FROM PessoaRelacionamento where relacionamento "
 			+ "in (SELECT relacionamento FROM PessoaRelacionamento WHERE pessoa.id =:idPessoa) AND nome is not null")
 	List<PessoaRelacionamento> retornaListaRelConj(@Param("idPessoa") Integer idPessoa);
+	
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("UPDATE PessoaRelacionamento r SET r.pessoa =:pessoa WHERE r.id =:id")
+	void updatePessoaId(@Param("pessoa") Pessoa pessoa ,@Param("id") Integer id);
 
 }
