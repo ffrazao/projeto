01d9e6c18861344a29228659eca6576583c964a2
Adan <adansks@gmail.com>
2018-01-19 11:40:55 -0200

    Adicionar coleta na mesclagem e cpf

--- aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/formulario/ColetaDao.java
+++ aterwebsrv/src/main/java/br/gov/df/emater/aterwebsrv/dao/formulario/ColetaDao.java
@@ -3,6 +3,9 @@ package br.gov.df.emater.aterwebsrv.dao.formulario;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.gov.df.emater.aterwebsrv.modelo.ater.PropriedadeRural;
 import br.gov.df.emater.aterwebsrv.modelo.formulario.Coleta;
@@ -16,5 +19,12 @@ public interface ColetaDao extends JpaRepository<Coleta, Integer> {
 	List<Coleta> findByFormularioVersaoAndPropriedadeRural(FormularioVersao fv, PropriedadeRural propriedadeRural);
 
 	Coleta findOneByChaveSisater(String chaveColetaFormulario);
+	
+	@Query("SELECT c.id FROM Coleta c WHERE c.pessoa.id =:pessoa")
+	List<Integer> findOneByPessoa(@Param("pessoa") Integer pessoa);
+	
+	@Modifying(clearAutomatically = true)
+	@Query("UPDATE Coleta SET pessoa.id =:pessoa WHERE id =:id")
+	void UpdatePessoa(@Param("pessoa") Integer pessoa, @Param("id") Integer id);
 
 }
